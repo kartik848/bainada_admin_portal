8a15075ed904cdd7effcdebec4fe6d1764e4b6d2
Kartik Kale <kartikkale848@gmail.com>
2026-09-03 23:28:04 +0530

    fix: Synchronize web portal with clean role and alignment updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4254652442" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1577585057" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
